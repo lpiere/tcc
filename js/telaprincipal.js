6562df48371b1a1e650b173e0df98a75db1af6b4
Lpieri <42479462+lpiere@users.noreply.github.com>
2018-10-29 11:57:07 -0300

    pt

--- js/telaprincipal.js
+++ js/telaprincipal.js
@@ -1,14 +1,27 @@
 var table = document.getElementById("datatables");
+let atualiza = false
 
-firebase.database().ref('pessoa/').on('value', function(snapshot){
-    snapshot.forEach(function (item){
-        console.log(item.val().nome);
-        console.log(item.val().saldo);
-        let nome = item.val().nome;
-        let saldo = item.val().saldo;
-        adicionar_coluna(nome, saldo)
+
+function atualizar(){
+    if(atualiza){
+        atualiza = false;
+        window.location.replace("./telaprincipal.html");
+    }
+    setTimeout(console.log("asdasd"), 1000);
+    atualizando();
+    
+}
+function atualizando(){
+    firebase.database().ref('pessoa/').on('value', function(snapshot){
+        snapshot.forEach(function (item){
+            let nome = item.val().nome
+            let saldo = item.val().saldo
+            adicionar_coluna(nome, saldo)
+        });
     });
-});
+    atualiza = true;
+}
+
 function adicionar_coluna(nome, saldo){
     var tr = document.createElement("tr");
     var td = document.createElement("td");
