349d8b7aba41ab5f22a06035b8d6b072649171af
Lpieri <42479462+lpiere@users.noreply.github.com>
2018-10-30 18:35:53 -0300

    posarduino

--- js/telausuario.js
+++ js/telausuario.js
@@ -1,13 +1,15 @@
 let saldo = document.getElementById("saldo");
-const nome = require("./login.js");
+// const nome = require("./login.js");
+let nome = "TESTE"
 let disponivel;
+let idpessoa;
 
 firebase.database().ref('pessoa/').on('value', function(snapshot){
     snapshot.forEach(function (item){
         if(nome == item.val().nome){
-            console.log("esse é o nome1: " + item.val().saldo);
-            saldo.innerText = item.val().saldo;
-            disponivel = item.val().saldo;
+            saldo.innerText = item.val().saldo
+            disponivel = item.val().saldo
+            idpessoa = item.val().idpessoa
         }
     });
 });
@@ -27,9 +29,27 @@ function fechar(){
     let maquinaid = document.getElementById("maquinaid");
     if(maquinaid.value != ""){
         firebase.database().ref("/maquina/"+maquinaid.value+"/ligar").set("false");
+        subtrair();
         firebase.database().ref("/maquina/"+maquinaid.value+"/disponivel").set(0);
     }else{   
-        alert("coloque o numero da maquina")
-    }
+        alert("coloque o numero da maquina");
+    }   
+}
 
+function subtrair(){
+    let maquinaid = document.getElementById("maquinaid")
+    let servido;
+    firebase.database().ref('maquina/').on('value', function(snapshot){
+        snapshot.forEach(function (item){
+        if(item.val().maquinaid == maquinaid.value){
+            servido = item.val().servido
+            console.log("if")
+            console.log("servido: ", servido)
+            console.log("disponivel: ", item.val().disponivel)
+            firebase.database().ref("pessoa/"+idpessoa+"/saldo").set(parseInt(item.val().disponivel)-parseInt(servido));
+            firebase.database().ref("maquina/"+maquinaid.value+"/servido").set(0);
+            maquinaid.value = ""
+            }
+        });
+    });
 }
